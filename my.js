69df1cafb416377da80601b485068f488b895f6b
esportleague <58951999+esportleague@users.noreply.github.com>
2020-07-15 17:58:45 +0200

    Update my.js

--- my.js
+++ my.js
@@ -1,6 +1,6 @@
   function testGS(){
   
-  const url = "https://script.googleusercontent.com/macros/echo?user_content_key=yDZsmpyQ30Bv6EevVSF_5kzNKP2Ry81X-q8VyQ4pGLpyeNdXwUtridw3_ZzIXqdD3gB7GdE7M15w_a1wwysdZ5qRSUE5CcxXm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnD3nwiGdpHCEDMENffCu3FTz9TyPyTEPUXh6QYTKzrrfcvl5SYHp0flIdZi-nyhfEcrT2JDZYYW7&lib=MBsrXmou5jn3bvnyeNR826nWkdjhOZoOc";
+  const url = "https://script.google.com/macros/s/AKfycbw2uYTKeWRm--pdEcy5ynRELWCT_N7ThXeX9Xtas9QSpw97a0fa/exec";
   
   fetch(url)
   .then(d=> d.json())
